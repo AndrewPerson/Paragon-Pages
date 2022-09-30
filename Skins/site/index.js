e57d5eddfbae42a46ed164edfba0ca7e25f9f955
AndrewPerson <68579649+AndrewPerson@users.noreply.github.com>
2022-09-30 10:53:29 +1000

    Added Code Mirror for better css editing

---
+++ Skins/site/index.js
@@ -0,0 +1,23 @@
+import { Init } from "https://paragon.pages.dev/js/paragon.js"
+
+Init();
+
+import { EditorView, basicSetup } from "codemirror";
+import { keymap } from "@codemirror/view";
+import { indentWithTab } from "@codemirror/commands";
+import { css } from "@codemirror/lang-css";
+
+let view = new EditorView({
+    extensions: [basicSetup, keymap.of(indentWithTab), css()],
+    parent: document.getElementById("editor")
+});
+
+document.getElementById("upload-css").addEventListener("click", e => {
+    window.parent.postMessage({
+        command: "Register Skin",
+        data: {
+            name: "Custom Skin",
+            css: view.state.doc.toJSON().join("\n")
+        }
+    }, "*");
+});
